c211c6aa06627fed2409865f3048c748ddc05c7b
From <from-wh@hotmail.com>
2020-07-28 14:31:19 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <h1>this is React</h1>
+      </div>
+      <div>
+        <h2>this is React2</h2>
+      </div>
+    </Fragment>
   );
 }
 
